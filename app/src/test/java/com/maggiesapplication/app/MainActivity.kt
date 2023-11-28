1b330cf62e1b9a0b5ef3d87e12d7722929a9aa1d
maggieh29 <rationalnpc@gmail.com>
2023-11-28 18:46:24 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/maggiesapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.maggiesapplication.app
+
+import com.maggiesapplication.app.appcomponents.base.BaseActivity
+import com.maggiesapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
